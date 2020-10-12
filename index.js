d89cd02fd095304342d0af64d98bf6cd3b048373
ullagjes <ullagjes@gmail.com>
2020-10-12 09:04:57 +0200

    lagt inn forslag til produktside

--- index.js
+++ index.js
@@ -1,23 +1,166 @@
 // Dette trenger jeg at javascripten min gjør:
 
 
-// Handlekurven min skal være en tom array. Dermed trenger jeg en funksjon som gjør at når jeg trykker på knappen
-// "kjøp", legger den til navnet på varen, bildet og prisen. Her må jeg lage en universell funksjon med gitte 
-// parametere, slik at jeg kan koble ulike knapper til den samme funksjonen, men legge til de unike parameterne
-// for den spesifikke varen. Dermed på knappene mine være unike og angi parameteren som skal legges inn i 
-// handlekurv-arrayet. 
-// Inne i handlekurven - som vil være en div i html-dokumentet mitt, må prisen for hver vare som legges til, 
-// summeres. Dette kan antakelig legges til samme funksjon som ovenfor. 
-// Det skal også være mulig å slette en vare, og dermed må summen i handlevognen endre seg. Altså må en ny knapp 
-// opprettes for hver gang arrayet oppdateres med en ny vare. 
+// Handlekurven min skal være en tom array. Dermed trenger jeg en funksjon som pusher inn et nytt objekt i handlekurv-arrayetnår jeg trykker på kjøpknappen. Jeg vil lage hvert objekt på forhånd hvor innholdet har tre hoved-keys: navn på produktet, pris og bilde. Bildene må lastes opp i egen img-mappe. Dermed kan jeg også bruke disse objektene til å fylle inn nettstedet mitt. 
 
-const handlekurv = document.querySelector("#handlekurv");
+const MINE_VARER = document.querySelector("#mine-varer");
+const MIN_HANDLEKURV = document.querySelector("#handlekurv");
+let handlekurv = [];
 
+//HENTER ALLE PRODUKTENE OG GENERER FORSIDE
 
-function handle(vare, pris) {
-    let handlekurvInnhold = [];
-    handlekurvInnhold.push(vare, pris);
-    console.log(handlekurvInnhold);
+let mineProdukter = [
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 1", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 2", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 3", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 4", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 5", 
+    pris: 100
+    },
+    {
+    bilde: "./images/test-bilde.jpg", 
+    produkt: "Vare 6", 
+    pris: 100
+    },
+    {
+    bilde: "./images/test-bilde.jpg", 
+    produkt: "Vare 7", 
+    pris: 100
+    },
+    {
+    bilde: "./images/test-bilde.jpg", 
+    produkt: "Vare 8", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 9", 
+    pris: 100
+    },
+    {
+    bilde:"./images/test-bilde.jpg",
+    produkt: "Vare 10", 
+    pris: 100
+    },
+    {
+    bilde: "./images/test-bilde.jpg", 
+    produkt: "Vare 11", 
+    pris: 100
+    },
+    {
+    bilde: "./images/test-bilde.jpg", 
+    produkt: "Vare 12", 
+    pris: 100
+    }
+];
+
+function forside () {
+    let html = ``;
+    mineProdukter.forEach(el => {
+        html += `
+            <div class="vare">
+                <img src="${el.bilde}">
+                <h2 class="varenavn">${el.produkt}</h2>
+                <div class="priceAndBuy">
+                    <h3 class="pris">${el.pris}</h3>
+                    <button class="buyBtn">Legg i handlekurv</button>
+                </div>
+            </div>
+            `;
+    });
+    MINE_VARER.innerHTML = html;
 }
 
-handle("kjole", 200);
+forside();
+
+// LEGG TIL VARE I HANDLEKURV
+
+let knapper = document.querySelectorAll(".buyBtn");
+for(const knapp of knapper) {
+    knapp.addEventListener("click", leggTilVare)
+}
+
+function leggTilVare(event) {
+
+    let knapp = event.target;
+    let trykketInnhold = knapp.parentElement.parentElement;
+    let minVare = trykketInnhold.querySelector(".varenavn").innerText;
+    let minPris = trykketInnhold.querySelector(".pris").innerText;
+    let mittBilde = trykketInnhold.querySelector("img").src;
+    
+    handlekurv.push(
+        {
+        bilde: mittBilde,
+        vare: minVare, 
+        pris: minPris
+        }
+        );
+
+
+    let html = ``;
+    handlekurv.forEach(el => {
+        html +=`
+        <div class="handlekurvElement">
+                <img src=${el.bilde}>
+                <span class="handlekurvProdukt">${el.vare}</span>
+                <span class="handlekurvPris">${el.pris}</span>
+                <input class="handlekurvKvantitet" type="number"> 
+                <button class="fjernFraHandlekurv">Fjern</button>
+            </div>
+            `
+    });
+
+    MIN_HANDLEKURV.innerHTML = html;
+
+    let fjernKnapper = document.querySelectorAll(".fjernFraHandlekurv");
+
+    for(const fjernKnapp of fjernKnapper) {
+    fjernKnapp.addEventListener("click", fjernVare);
+    }   
+
+    console.log(handlekurv);
+}
+
+//FJERNKNAPPER - denne funksjonen fjerner både innholdet i arrayet og html-innholdet i handlekurven
+
+function fjernVare(event) {
+    let fjernObjekt = event.target;
+    fjernObjekt = fjernObjekt.parentElement.remove();
+    fjernObjekt = handlekurv.shift();
+    console.log(handlekurv);
+}
+
+
+
+
+
+
+// grunnleggende struktur for funksjon til flere knapper
+
+/*let knapper = document.querySelectorAll(".buyBtn");
+
+for(const knapp of knapper) {
+    knapp.addEventListener("click", test)
+}
+
+function test() {
+    console.log("hei");
+}*/
